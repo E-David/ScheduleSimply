974e6a29248e51f896a2066235b2294c390a2516
E-David <ericdavid365@gmail.com>
2017-01-06 14:45:28 -0600

    add heroku url to redirect and client secret

--- client_secret_2.js
+++ client_secret_2.js
@@ -1,13 +1,13 @@
 var secrets = {
 	"web":{
-		"client_id": "587179870005-4t54t2sn7peb3nf6rcpa6q92ottds8kq.apps.googleusercontent.com",
-		"project_id": "inspired-access-149420",
-		"auth_uri": "https://accounts.google.com/o/oauth2/auth",
-		"token_uri": "https://accounts.google.com/o/oauth2/token",
-		"auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-		"client_secret": "DndlyDqbo-fq6MqFro7tbTK2",
-		"redirect_uris": ["http://localhost:3000/google/calendar/code","https://schedule-simply.herokuapp.com/google/calendar/code"],
-		"javascript_origins": ["http://localhost:3000","https://schedule-simply.herokuapp.com"]
+		"client_id":"587179870005-4t54t2sn7peb3nf6rcpa6q92ottds8kq.apps.googleusercontent.com",
+		"project_id":"inspired-access-149420",
+		"auth_uri":"https://accounts.google.com/o/oauth2/auth",
+		"token_uri":"https://accounts.google.com/o/oauth2/token",
+		"auth_provider_x509_cert_url":"https://www.googleapis.com/oauth2/v1/certs",
+		"client_secret":"DndlyDqbo-fq6MqFro7tbTK2",
+		"redirect_uris":["http://localhost:3000/google/calendar/code","https://schedulesimply.herokuapp.com/google/calendar/code"],
+		"javascript_origins":["http://localhost:3000","https://schedulesimply.herokuapp.com"]
 	}
 }
 
