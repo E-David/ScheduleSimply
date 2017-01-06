832b74828502f78c381c0ec04daa2523f0fc3121
E-David <ericdavid365@gmail.com>
2017-01-06 17:38:56 -0600

    change google url

--- src/scripts/views/loginView.js
+++ src/scripts/views/loginView.js
@@ -2,7 +2,7 @@ import React from "react"
 import ACTIONS from "../actions"
 
 const LoginView = React.createClass({
-	authUrl: "https://accounts.google.com/o/oauth2/auth?scope=https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fcalendar&access_type=offline&response_type=code&client_id=587179870005-4t54t2sn7peb3nf6rcpa6q92ottds8kq.apps.googleusercontent.com&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2Fgoogle%2Fcalendar%2Fcode",
+	authUrl: "https://accounts.google.com/o/oauth2/auth?scope=https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fcalendar&access_type=offline&response_type=code&client_id=587179870005-4t54t2sn7peb3nf6rcpa6q92ottds8kq.apps.googleusercontent.com&redirect_uri=https%3A%2F%2Fschedulesimply.herokuapp.com%2Fgoogle%2Fcalendar%2Fcode",
 	_googleAuth: function(event) {
 		event.preventDefault()
 		window.location.replace(this.authUrl)
